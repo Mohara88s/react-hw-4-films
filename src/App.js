ce5515140da30256c9b650f0cb8ebf58301b1c7a
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-23 14:59:15 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import 'modern-normalize/modern-normalize.css';
-import styles from './App.module.css';
-
-class App extends Component {
-  state = {};
-
-  render() {
-    return <div className={styles.container}></div>;
-  }
-}
-
+const HomePage = () => <h2>HomePage</h2>;
+const MoviesPage = () => <h2>MoviesPage</h2>;
+const App = () => (
+  <Switch>
+    <Route path="/">
+      <HomePage />
+    </Route>
+    <Route path="/movies">
+      <MoviesPage />
+    </Route>
+  </Switch>
+);
 export default App;
+
+// https://api.themoviedb.org/3/movie/550?api_key=618d26280c3d16ba0e8ea1b5e615dd52
+// import HomePage from './views/HomePage';
+// import MoviesPage from './views/MoviesPage';
+// import 'modern-normalize/modern-normalize.css';
+// import styles from './App.module.css';
---
+++ src/views/MoviesPage.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const MoviesPage = () => <h2>MoviesPage</h2>;
+export default MoviesPage;
