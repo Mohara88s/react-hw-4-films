96d9a5f01d921e21f10307b185614ba60c38c78b
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-23 10:58:16 +0300

    project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import 'modern-normalize/modern-normalize.css';
+import styles from './App.module.css';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return <div className={styles.container}></div>;
+  }
+}
+
+export default App;
